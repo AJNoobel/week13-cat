3d9a3d9b1f623ec8ab893d157924fcb23c0fad97
Opilane TTHK <opilane@tthk.ee>
2021-11-24 13:04:36 +0200

    yes

---
+++ oopcat/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace oopcat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hungriness;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungriness = 0;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+            public double Hungriness
+            {
+                get { return hungriness; }
+            }
+
+            public void Sleeps()
+            {
+                Console.WriteLine("Z-z-z-z-z-Z");
+                hungriness += 0.2;
+            }
+
+            public void meow()
+            {
+                Console.WriteLine($"{name} says \"eee choza\"");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat("Garfield", "ginger");
+
+            while (myCat.Hungriness != 1)
+            {
+                myCat.Sleeps();
+            }
+
+            myCat.meow();
+        }
+    }
+}
